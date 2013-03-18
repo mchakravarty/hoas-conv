ada6ee6a5523c9ee2c37874801094c49cb1162ff
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2013-03-18 21:35:21 +1100

    De Bruijn conversion for terms with sharing recovery

--- Convert.hs
+++ Convert.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, ScopedTypeVariables #-}
 
-module Convert (convert) where
+module Convert (convert, convertSharing) where
 
+  -- standard libraries
+import Data.List
 import Data.Typeable
 import Data.Maybe
 
+  -- friends
 import qualified DeBruijn
 import qualified HOAS
+import Sharing
 
 
 -- A layout of an environment an entry for each entry of the environment.
@@ -16,7 +20,7 @@ import qualified HOAS
 data Layout env env' where
   EmptyLayout :: Layout env ()
   PushLayout  :: Typeable t 
-              => Layout env env' -> DeBruijn.Ix env t -> Layout env (env', t)
+              => Layout env env' -> DeBruijn.Idx env t -> Layout env (env', t)
 
 -- Yield the number of entries in an environment layout
 --
@@ -28,26 +32,66 @@ size (PushLayout lyt _) = size lyt + 1
 --
 inc :: Layout env env' -> Layout (env, t) env'
 inc EmptyLayout         = EmptyLayout
-inc (PushLayout lyt ix) = PushLayout (inc lyt) (DeBruijn.SuccIx ix)
+inc (PushLayout lyt ix) = PushLayout (inc lyt) (DeBruijn.SuccIdx ix)
 
--- Project the nth index out of an environment layout
+-- Project the nth index out of an environment layout.
 --
-prjIx :: Typeable t => Int -> Layout env env' -> DeBruijn.Ix env t
-prjIx _ EmptyLayout       = error "Convert.prjIx: internal error"
-prjIx 0 (PushLayout _ ix) = fromJust (gcast ix)
-                              -- can't go wrong unless the library is wrong!
-prjIx n (PushLayout l _)  = prjIx (n - 1) l
+-- All errors are internal errors.
+--
+prjIdx :: forall t env env'. Typeable t => String -> Int -> Layout env env' -> DeBruijn.Idx env t
+prjIdx ctxt 0 (PushLayout _ (idx :: DeBruijn.Idx env0 t0))
+  = case gcast idx of
+      Just idx' -> idx'
+      Nothing   -> 
+        error $ "Convert.prjIdx: type mismatch at " ++ ctxt ++ "\n  " ++
+                "Couldn't match expected type `" ++ show (typeOf (undefined::t)) ++ 
+                "' with actual type `" ++ show (typeOf (undefined::t0)) ++ "'"
+prjIdx ctxt n (PushLayout l _)  = prjIdx ctxt (n - 1) l
+prjIdx ctxt _ EmptyLayout       = error $ "Convert.prjIdx: environment doesn't contain index at " ++ ctxt
 
--- Convert a HOAS term -always a closed term- to a closed de Bruijn term
+-- |Convert a closed HOAS term to a closed de Bruijn term.
 --
 convert :: HOAS.Term t -> DeBruijn.Term () t
 convert = cvt EmptyLayout
   where
     cvt :: Layout env env -> HOAS.Term t -> DeBruijn.Term env t
-    cvt lyt (HOAS.Tag sz)      = DeBruijn.Var (prjIx (size lyt - sz - 1) lyt)
+    cvt lyt (HOAS.Tag sz)      = DeBruijn.Var (prjIdx "'Tag' in vanilla convert" (size lyt - sz - 1) lyt)
     cvt lyt (HOAS.Con v)       = DeBruijn.Con v
     cvt lyt (HOAS.Lam f)       = DeBruijn.Lam (cvt lyt' (f tag))
       where
         tag  = HOAS.Tag (size lyt)
-        lyt' = inc lyt `PushLayout` DeBruijn.ZeroIx
+        lyt' = inc lyt `PushLayout` DeBruijn.ZeroIdx
     cvt lyt (HOAS.App fun arg) = DeBruijn.App (cvt lyt fun) (cvt lyt arg)
+
+-- |Convert a closed HOAS term to a closed de Bruijn term while recovering the sharing of the
+-- source expression.
+--
+convertSharing :: Typeable t => HOAS.Term t -> DeBruijn.Term () t
+convertSharing = cvt EmptyLayout [] . recoverSharing
+  where
+    cvt :: Layout env env -> [StableSharingTerm] -> SharingTermFloated t -> DeBruijn.Term env t
+    cvt lyt env (VarSharing st)
+      | Just i <- findIndex (matchStableTerm st) env
+      = DeBruijn.Var (prjIdx (ctxt ++ "; i = " ++ show i) i lyt)
+      | null env
+      = error $ "Cyclic definition of a term (st = " ++ show (hashStableTermHeight st) ++ ")"
+      | otherwise                                   
+      = error $ "convertSharing: " ++ err
+      where
+        ctxt = "shared term with stable name " ++ show (hashStableTermHeight st)
+        err  = "inconsistent valuation @ " ++ ctxt ++ ";\n  env = " ++ show env
+    cvt lyt env (LetSharing st@(StableSharingTerm _ boundTerm) bodyTerm)
+      = let lyt' = inc lyt `PushLayout` DeBruijn.ZeroIdx
+        in
+        DeBruijn.Let (cvt lyt env boundTerm) (cvt lyt' (st:env) bodyTerm)
+    cvt lyt env (TermSharing _ (Tag lvl)) 
+      = DeBruijn.Var (prjIdx ("de Bruijn conversion tag " ++ show lvl) lvl lyt)
+    cvt lyt env (TermSharing _ (Con v))
+      = DeBruijn.Con v
+    cvt lyt env (TermSharing _ (Lam sts f))
+      = DeBruijn.Lam (cvt lyt' env' f)
+      where
+        lyt' = inc lyt `PushLayout` DeBruijn.ZeroIdx
+        env' = sts:env
+    cvt lyt env (TermSharing _ (App fun arg)) 
+      = DeBruijn.App (cvt lyt env fun) (cvt lyt env arg)
--- DeBruijn.hs
+++ DeBruijn.hs
@@ -12,33 +12,35 @@ import Data.Char
 -- Index projecting a specific type from a type environment; it's value
 -- corresponds to a natural number, the de Brujin index
 --
-data Ix env t where
-  ZeroIx ::             Ix (env, t) t
-  SuccIx :: Ix env t -> Ix (env, s) t
+data Idx env t where
+  ZeroIdx ::              Idx (env, t) t
+  SuccIdx :: Idx env t -> Idx (env, s) t
 
-instance Show (Ix env t) where
-  show = show . ixToInt
+instance Show (Idx env t) where
+  show = show . idxToInt
 
-ixToInt :: Ix env t -> Int
-ixToInt ZeroIx     = 0
-ixToInt (SuccIx n) = ixToInt n + 1
+idxToInt :: Idx env t -> Int
+idxToInt ZeroIdx     = 0
+idxToInt (SuccIdx n) = idxToInt n + 1
 
 -- Lambda terms using de Bruijn indices to represent variables
 --
 data Term env t where
-  Var :: Ix env t                        -> Term env t
+  Var :: Idx env t                       -> Term env t
   Con :: Show t
       => t                               -> Term env t
   Lam :: Term (env, s) t                 -> Term env (s -> t)
   App :: Term env (s -> t) -> Term env s -> Term env t
+  Let :: Term env s -> Term (env, s) t   -> Term env t
 
 instance Show (Term env t) where
   show = showTerm
     where
-      showTerm (Var ix)      = "#" ++ show ix
-      showTerm (Con c)       = show c
-      showTerm (Lam body)    = "\\" ++ show body
-      showTerm (App fun arg) = showParen fun ++ " " ++ showParen arg
+      showTerm (Var ix)       = "#" ++ show ix
+      showTerm (Con c)        = show c
+      showTerm (Lam body)     = "\\" ++ show body
+      showTerm (App fun arg)  = showParen fun ++ " " ++ showParen arg
+      showTerm (Let bnd body) = "let " ++ show bnd ++ " in " ++ show body
       
       showParen t@(Var {}) = show t
       showParen t@(Con {}) = show t
@@ -48,22 +50,24 @@ pprTerm :: Term env t -> String
 pprTerm = ppr 0
   where
     ppr :: Int -> Term env t -> String
-    ppr lvl (Var ix)      = pprIx lvl ix
+    ppr lvl (Var ix)      = pprIdx lvl ix
     ppr lvl (Con c)       = show c
-    ppr lvl (Lam body)    = "\\" ++ pprIx lvl ZeroIx ++ ". " ++ ppr (lvl + 1) body
+    ppr lvl (Lam body)    = "\\" ++ pprIdx lvl ZeroIdx ++ ". " ++ ppr (lvl + 1) body
     ppr lvl (App fun arg) = pprParen lvl fun ++ " " ++ pprParen lvl arg
+    ppr lvl (Let bnd body) = "let " ++ pprIdx lvl ZeroIdx ++ " = " ++ ppr lvl bnd ++
+                             " in " ++ ppr (lvl + 1) body
     
     pprParen :: Int -> Term  env t -> String
     pprParen lvl t@(Var {}) = ppr lvl t
     pprParen lvl t@(Con {}) = ppr lvl t
     pprParen lvl t          = "(" ++ ppr lvl t ++ ")"
     
-    pprIx :: Int -> Ix env t -> String
-    pprIx lvl ix
+    pprIdx :: Int -> Idx env t -> String
+    pprIdx lvl idx
       | n < 26    = [chr (ord 'a' + n)]
       | otherwise = 'v':show n
       where
-        n = lvl - ixToInt ix
+        n = lvl - idxToInt idx
 
 -- Valuation for a type environment
 --
@@ -73,14 +77,15 @@ data Val env where
 
 -- Projection of a value from a valuation using a de Bruijn index
 --
-prj :: Ix env t -> Val env -> t
-prj ZeroIx      (Push val v) = v
-prj (SuccIx ix) (Push val _) = prj ix val
+prj :: Idx env t -> Val env -> t
+prj ZeroIdx       (Push val v) = v
+prj (SuccIdx idx) (Push val _) = prj idx val
 
 -- A term interpreter, evaluating a term under a valuation
 --
 intp :: Term env t -> Val env -> t
-intp (Var ix)      val = prj ix val
-intp (Con v)       val = v
-intp (Lam body)    val = intp body . (val `Push`)
-intp (App fun arg) val = (intp fun val) (intp arg val)
+intp (Var ix)       val = prj ix val
+intp (Con v)        val = v
+intp (Lam body)     val = intp body . (val `Push`)
+intp (App fun arg)  val = (intp fun val) (intp arg val)
+intp (Let bnd body) val = intp body (val `Push` intp bnd val)
