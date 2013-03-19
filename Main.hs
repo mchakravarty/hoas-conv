7156bba3a431ccbe19d7aad4861a5660e6d440df
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2013-03-19 23:09:32 +1100

    Some sharing tests

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoMonomorphismRestriction, ExtendedDefaultRules #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
 import Data.Typeable
@@ -23,6 +23,8 @@ plus = HOAS.lam $ \m ->
 
 plusTwoThree = plus `HOAS.app` two `HOAS.app` three
 
+plusTwoTwoLet = let a = two in plus `app` a `app` a
+
 data Nat = Z | S Nat deriving (Show, Typeable)
 
 pair = HOAS.lam $ \x -> 
@@ -37,7 +39,7 @@ pairfstPair = pairfst `HOAS.app`
 testSharing :: (Eq t, Show t, Typeable t) => HOAS.Term t -> IO ()
 testSharing t
   | sharingResult == nonSharingResult 
-  = putStrLn $ DeBruijn.pprTerm (convert t) ++ ": OK"
+  = putStrLn $ DeBruijn.pprTerm (convertSharing t) ++ ": OK"
   | otherwise
   = do
     { putStrLn $ DeBruijn.pprTerm (convert t) ++ ": OK"
@@ -50,18 +52,32 @@ testSharing t
 
 main 
   = do
-      printLine "Identity           :"     (convert i)
-      printLine "zero               :"     (convert zero)
-      printLine "one                :"     (convert one)
-      printLine "two                :"     (convert two)
-      printLine "three              :"     (convert three)
-      printLine "plus               :"     (convert plus)
-      printLine "plus two three     :\n  " (convert plusTwoThree)
+      printLine "Identity               :"     (convert i)
+      printLine "zero                   :"     (convert zero)
+      printLine "one                    :"     (convert one)
+      printLine "two                    :"     (convert two)
+      printLine "three                  :"     (convert three)
+      printLine "plus                   :"     (convert plus)
+      printLine "plus two three         :\n  " (convert plusTwoThree)
+      printLine "let x = two in plus x x:\n  " (convert plusTwoTwoLet)
+      printLine "let x = two in plus x x:\n  " (convertSharing plusTwoTwoLet)
+      putStrLn  "  (with sharing)"
       printLine' "EVAL plus two three:" 
         ((DeBruijn.intp (convert plusTwoThree)) DeBruijn.Empty S Z)
       printLine "pairfst (pair 'a' 'b'):\n  " (convert pairfstPair)
       printLine' "EVAL pairfst (pair 'a' 'b'):"   
         (DeBruijn.intp (convert pairfstPair) DeBruijn.Empty)
+
+      testSharing (let two = con 2 in con (+) `app` two `app` two)
+      testSharing (let four = let two = con 2 in con (+) `app` two `app` two
+                   in
+                   con (*) `app` four `app` four)
+        -- -- let y = \a -> (1::Int) in let x = let z = (\c d -> c) y in z z in x ((\b -> x) y)
+        -- let y = \a -> (1::Int) in let x = let z = (\c -> c) y in (\d e -> d) z z in x ((\b -> x) y)
+      testSharing (let    y = lam $ \a -> con (1::Int) 
+                   in let x = let z = (lam $ \c -> c) `app` y in (lam $ \d -> lam $ \e -> d) `app` z `app` z 
+                   in 
+                   (lam $ \d -> lam $ \e -> d `app` con (2::Int)) `app` x `app` ((lam $ \b -> x) `app` y))
   where
     printLine  desc e = putStrLn $ desc ++ " " ++ show e ++ "    — " ++ DeBruijn.pprTerm e
     printLine' desc e = putStrLn $ desc ++ " " ++ show e
